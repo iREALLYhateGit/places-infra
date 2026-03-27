4d0e3ffd4a01e1cba82a7304a7f891c5273ae16a
Anonen Stepan <s.anonen@yandex.ru>
2026-03-27 15:11:29 +0300

    new branch

--- lab3/monitoring/monitoring.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm install monitoring prometheus-community/kube-prometheus-stack \
-  --namespace monitoring \
-  --create-namespace \
-  --set grafana.service.type=NodePort \
-  --set grafana.service.nodePort=30300
--- lab3/terraform/kubeconfig.tf
+++
@@ -1,7 +0,0 @@
-resource "null_resource" "kubeconfig" {
-  depends_on = [yandex_kubernetes_cluster.k8s-cluster]
-
-  provisioner "local-exec" {
-    command = "yc managed-kubernetes cluster get-credentials --name k8s-cluster --external --force"
-  }
-}
