85dda6c1031166921af77a3725a89103e7d72f8e
Anonen Stepan <s.anonen@yandex.ru>
2026-03-29 21:05:25 +0300

    create folders terraforn & ansible

---
+++ lab4/terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "zone" {
+  type    = string
+  default = "ru-central1-a"
+}
+
+variable "cloud_id" {
+  type = string
+}
+
+variable "folder_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
